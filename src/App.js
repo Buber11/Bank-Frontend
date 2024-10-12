5a3d5d30b8323c064830da5bab03007f11c1db89
Buber11 <105680119+Buber11@users.noreply.github.com>
2024-10-12 18:30:58 +0200

    add contactPage

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import Navbar from './components/common/Navbar'; // Import komponentu Navbar
 import LoginPage from './components/loginPage/LoginPage'; // Import strony logowania
 import AboutPage from './components/aboutPage/AboutPage';
 import Footer from './components/common/Footer';
+import ContactPage from './components/contact/ContactPage';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+        
         <Route path="/login" element={<LoginPage />} />
         <Route path="/about" element={<AboutPage/>} />
+        <Route path="/contact" element={<ContactPage/>} />
       </Routes>
       <Footer></Footer>
     </Router>
---
+++ src/components/contact/ContactPage.js
@@ -0,0 +1,28 @@
+// src/Components/ContactPage/Contact.js
+import React from 'react';
+import './ContactPage.css'; // Import the CSS for the Contact page
+
+const Contact = () => {
+    return (
+        <div className="contact-container">
+            <h1>Contact Us</h1>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id="name" name="name" required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="message">Message</label>
+                    <textarea id="message" name="message" rows="5" required></textarea>
+                </div>
+                <button type="submit" className="submit-button">Send Message</button>
+            </form>
+        </div>
+    );
+};
+
+export default Contact;
