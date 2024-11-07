64783d27dd90c37566db5f69575dc86f131afd30
Buber11 <105680119+Buber11@users.noreply.github.com>
2024-11-07 16:06:09 +0100

    refactor ( navbar )

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../AuthContext';
+import './Navbar.css'
 
 const Navbar = () => {
     const { isLoggedIn, role, logout } = useAuth();
